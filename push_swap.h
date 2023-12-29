1c27cb7d552189a1aa39f98468fe341604e3a88d
Iryna Stasheuski <istasheu@student.hive.fi>
2023-12-29 17:22:07 +0200

    middle_step_2: asigning index

--- push_swap.h
+++ push_swap.h
@@ -16,6 +16,7 @@
 # include <stdio.h>
 # include <unistd.h>
 # include <stdlib.h>
+# include <limits.h>
 
 typedef struct s_stack
 {
--- stack.c
+++ stack.c
@@ -6,7 +6,7 @@
 /*   By: istasheu <istasheu@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/29 11:07:56 by istasheu          #+#    #+#             */
-/*   Updated: 2023/12/29 15:40:03 by istasheu         ###   ########.fr       */
+/*   Updated: 2023/12/29 17:21:10 by istasheu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,6 +48,35 @@ int	stk_len(t_stack *stk)
 		len++;
 	}
 	return (len);
+}
+
+void	add_index(t_stack *stk_a, int stk_len)
+{
+	t_stack *ptr;
+	t_stack	*highest;
+	int		value;
+
+	while (stk_len > 0)
+	{
+		ptr = stk_a;
+		highest = NULL;
+		value = INT_MAX;
+		while(ptr)
+		{
+			if (ptr->value == INT_MAX && ptr->index == 0)
+				ptr->index == 1;
+			if (ptr->value > value && ptr->index == 0)
+			{
+				value = ptr->value;
+				highest = ptr;
+				ptr = stk_a;
+			}
+			else
+				ptr = ptr->next;
+			if (hghest != NULL)
+				highest->index = stk_len;
+		}
+	}
 }
 
 t_stack	*stack_new(int value)
