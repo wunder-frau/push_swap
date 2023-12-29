e50e9ce86d6df847dc4f73d6caae488af2db2da0
Iryna Stasheuski <istasheu@student.hive.fi>
2023-12-29 10:27:46 +0200

    develop: empty files

---
+++ main.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: istasheu <istasheu@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/29 09:19:40 by istasheu          #+#    #+#             */
+/*   Updated: 2023/12/29 10:21:34 by istasheu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+int	main(int argc, char *argv)
+{
+	t_stack	*stk_a;
+	t_stack *stk_b;
+	int		stack_size;
+
+	if (ac < 2)
+		retirn (0);
+	stk_b = NULL;
+	return (0);
+}
---
+++ push_swap.h
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: istasheu <istasheu@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/28 10:02:32 by istasheu          #+#    #+#             */
+/*   Updated: 2023/12/29 10:25:30 by istasheu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+# include <stdio.h>
+# include <unistd.h>
+# include <stdlib.h>
+
+typedef struct s_stack
+{
+	int		value;
+	int		index;
+	int		position;
+	struct	s_stack *next;
+} t_stack;
+#endif
